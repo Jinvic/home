4ee25f3922e5758e8d77149bbca58bd2ba7a9c23
jwb <jiangwenbin@readboy.com>
2024-12-19 08:53:29 +0800

    网站链接 添加图床

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Desktop, Key } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Desktop, Key, Image } from "@vicons/fa"; // 注意使用正确的类别
 import { Message } from "@vicons/tabler"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -75,6 +75,7 @@ const siteIcon = {
   Microblog,
   Desktop,
   Key,
+  Image,
 };
 
 // 链接跳转
