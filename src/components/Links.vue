bb32ceb71709d241354b57d24941e8ad9ea2e2a6
Jinvic <2528976435@qq.com>
2025-02-06 16:54:27 +0800

    网站链接 添加邮箱

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen, Envelope } from "@vicons/fa"; // 注意使用正确的类别
 import { DesktopSignal24Regular } from "@vicons/fluent"; // 注意使用正确的类别
 import { DashboardOutlined } from "@vicons/antd"; // 注意使用正确的类别
 // import { Message } from "@vicons/tabler"; // 注意使用正确的类别
@@ -85,6 +85,7 @@ const siteIcon = {
   DesktopSignal24Regular,
   BookOpen,
   ChatBot,
+  Envelope,
 };
 
 // 链接跳转
