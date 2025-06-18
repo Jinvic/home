5af833ff8563c247d1dac2decead20b9adf07a60
Jinvic <2528976435@qq.com>
2025-06-18 16:37:13 +0800

    网站链接 分享 | FileCodeBox 影视 | LibreTV 优化 | PromptOptimizer 内网 | Frp

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,12 +43,12 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen, Envelope } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen, Envelope, ShareSquare, PlayCircle, AlignLeft } from "@vicons/fa"; // 注意使用正确的类别
 import { DesktopSignal24Regular } from "@vicons/fluent"; // 注意使用正确的类别
 import { DashboardOutlined } from "@vicons/antd"; // 注意使用正确的类别
 import { Message, Books } from "@vicons/tabler"; // 注意使用正确的类别
 import { DrawRound } from "@vicons/material"; // 注意使用正确的类别
-import { Dashboard, ChatBot } from "@vicons/carbon"; // 注意使用正确的类别
+import { Dashboard, ChatBot, UserAccess } from "@vicons/carbon"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -88,6 +88,10 @@ const siteIcon = {
   Envelope,
   Books,
   DrawRound,
+  ShareSquare,
+  PlayCircle,
+  AlignLeft,
+  UserAccess
 };
 
 // 链接跳转
