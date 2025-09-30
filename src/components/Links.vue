46c591c3f0246030765f6741388bbc4fb26a97a6
Jinvic <2528976435@qq.com>
2025-09-30 16:15:38 +0800

    较大更新

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,12 +43,12 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen, Envelope, ShareSquare, PlayCircle, AlignLeft } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Comment, Microblog, Key, Image, Fingerprint, BookOpen, Envelope, ShareSquare, PlayCircle, AlignLeft, Sync, ShareSquareRegular } from "@vicons/fa"; // 注意使用正确的类别
 import { DesktopSignal24Regular } from "@vicons/fluent"; // 注意使用正确的类别
 import { DashboardOutlined } from "@vicons/antd"; // 注意使用正确的类别
-import { Message, Books } from "@vicons/tabler"; // 注意使用正确的类别
+import { Books,ReportMoney } from "@vicons/tabler"; // 注意使用正确的类别
 import { DrawRound } from "@vicons/material"; // 注意使用正确的类别
-import { Dashboard, ChatBot, UserAccess } from "@vicons/carbon"; // 注意使用正确的类别
+import { Dashboard, ChatBot, UserAccess, Analytics } from "@vicons/carbon"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -91,7 +91,11 @@ const siteIcon = {
   ShareSquare,
   PlayCircle,
   AlignLeft,
-  UserAccess
+  UserAccess,
+  ShareSquareRegular,
+  Sync,
+  Analytics,
+  ReportMoney
 };
 
 // 链接跳转
